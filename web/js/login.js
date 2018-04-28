cadfe54d49ff7d351addf9ff9d1c3dc5b0c4cc9f
ccc <522978139@qq.com>
2018-04-28 18:06:44 +0800

    登录

---
+++ web/js/login.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    new Vue({
+        el: "#wrap",
+        mounted:function(){
+            var _this = this;
+            _this.$nextTick(function(){
+            });
+        }
+    });
+}
