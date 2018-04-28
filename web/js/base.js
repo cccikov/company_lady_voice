cf6b9b9c6fee403fe0bf0063ab750b1556974bab
ccc <522978139@qq.com>
2018-04-28 12:01:03 +0800

    细节修改

--- web/js/base.js
+++ web/js/base.js
@@ -1,34 +1,35 @@
 var bottom_template =
-'<div class="footer">'+
-    '<a :class="{active:footer==\'index\'}" href="index.html">首页</a>'+
-    '<a :class="{active:footer==\'overhear\'}" href="overhear.html">偷听</a>'+
-    '<a :class="{active:footer==\'msg\'}" href="msg.html">消息</a>'+
-    '<a :class="{active:footer==\'find\'}" href="find.html">发现</a>'+
-    '<a :class="{active:footer==\'me\'}" href="me.html">我的</a>'+
-'</div>'
+    '<div class="footer">' +
+    '<a :class="{active:footer==\'index\'}" href="index.html">首页</a>' +
+    '<a :class="{active:footer==\'overhear\'}" href="overhear.html">偷听</a>' +
+    '<a :class="{active:footer==\'msg\'}" href="msg.html">消息</a>' +
+    '<a :class="{active:footer==\'find\'}" href="find.html">发现</a>' +
+    '<a :class="{active:footer==\'me\'}" href="me.html">我的</a>' +
+    '</div>';
 
 Vue.component('bottom', {
     template: bottom_template,
-    data:function(){
+    data: function() {
         return {
-            footer:null
+            footer: null
         }
     },
-    mounted:function(){
+    mounted: function() {
         var _this = this;
-        _this.$nextTick(function(){
-            _this.footer = getPath();
+        _this.$nextTick(function() {
+            var path = getPath();
+            _this.footer = path || "index";
         });
     }
 });
 
-function getPath(){
+function getPath() {
     var path = document.location.pathname;
     var reg = /\/(\w+?).html/;
     var result = path.match(reg);
-    if(!!result){
+    if (!!result) {
         return result[1];
-    }else{
+    } else {
         return ""
     }
 }
--- web/js/me.js
+++ web/js/me.js
@@ -1,6 +1,14 @@
 window.onload = function() {
     new Vue({
         el: "#wrap",
+        data:{
+            overhead:true
+        },
+        methods:{
+            isOverhead:function(){
+                this.overhead = !this.overhead;
+            }
+        },
         mounted:function(){
             var _this = this;
             _this.$nextTick(function(){
